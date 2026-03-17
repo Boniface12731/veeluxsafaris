ee1a2a7ff8355a153bffac39fde2bb62df975570
Bonface Osuka <bonyosuks12731@gmail.com>
2026-03-17 16:51:38 +0300

    email

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -12,14 +12,13 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendEmail = async (req, res) => {
-    const { to, subject, text, attachments } = req.body;
+    const { to, subject, text } = req.body;
     const mailOptions = {
         from: process.env.EMAIL_USER,
         to,
         subject,
-        text,
-        attachments: attachments || []
-    };
+        text
+        };
     try {
         await transporter.sendMail(mailOptions);
         res.status(200).json({ success: true, message: 'Email sent successfully!' });
